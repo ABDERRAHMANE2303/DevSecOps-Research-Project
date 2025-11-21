b516b54af4deb7203c7c7d9828e1c56328b93010
abderrahmane <essahihabderrahman2020@gmail.com>
2025-11-21 23:40:07 +0100

    providers and variables

---
+++ infrastructure/variables.tf
@@ -0,0 +1,52 @@
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "name_prefix" {
+  description = "Prefix this project's resources"
+  type        = string
+  default     = "DevSecOpsProject"
+}
+
+variable "azs" {
+  description = "availability zones to use"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "admin_ip"{
+  description = "value of the current admin ip adress "
+  type = string 
+}
+
+variable "bastion_key_name" {
+  description = "EC2 key pair name to SSH into bastion"
+  type        = string
+}
+
+
+locals {
+  common_tags = {
+    Project = "DevSecOpsProject"
+    Owner   = "AQ"
+    Env     = "DevProd"
+  }
+  azs = var.azs
+  name_prefix = var.name_prefix
+}
+
+locals {
+  public_subnet_cidrs = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  private_subnet_cidrs = [
+    "10.0.3.0/24",
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/24"
+  ]
+}
